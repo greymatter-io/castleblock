49d7a37f213e9eec2efb75f5fbc0932216a86dfa
John Cudd <john.cudd@deciphernow.com>
2021-09-03 17:24:45 -0400

    Added logos, and cleaned up css

--- castleblock-ui/src/Card.svelte
+++ castleblock-ui/src/Card.svelte
@@ -21,62 +21,76 @@ export let icons = null;
 
 <div class="box">
   <div class="icon">
-{#if icons}
-        <picture >
-          {#each icons.reverse() as icon}
-            <source srcset="{`${pack.path}/latest/${icon.src}`}" />
-          {/each}
-          <img alt="logo" width="60px" />
-        </picture>
-      {/if}
+    {#if icons}
+      <picture>
+        {#each icons.reverse() as icon}
+          <source srcset="{`${pack.path}/latest/${icon.src}`}" />
+        {/each}
+        <img alt="logo" width="60px" />
+      </picture>
+    {/if}
   </div>
-  <details>
-    <summary>
-      
-      <a
-        target="_blank"
-        href="{`${window.location.origin}${pack.path}/latest/`}"
-        >{title || pack.name}</a
-      >
-      {#if description}
-        <div>{description}</div>
-      {/if}
-    </summary>
-    <p>
-      {#each pack.versions.reverse() as version, i}
-        <div>
-          <a
-            target="_blank"
-            href="{`${window.location.origin}${pack.path}/${version}/`}">
-            {pack.name} - {version}
-            {i == 0 ? "(latest)" : ""}
-          </a>
-        </div>
-      {/each}
-    </p>
-  </details>
+  <div class="right-side">
+    <details>
+      <summary>
+        <a
+          target="_blank"
+          href="{`${window.location.origin}${pack.path}/latest/`}"
+          >{title || pack.name}</a>
+        {#if description}
+          <div>{description}</div>
+        {/if}
+      </summary>
+      <p>
+        {#each pack.versions.reverse() as version, i}
+          <div>
+            <a
+              target="_blank"
+              href="{`${window.location.origin}${pack.path}/${version}/`}">
+              {pack.name} - {version}
+              {i == 0 ? "(latest)" : ""}
+            </a>
+          </div>
+        {/each}
+      </p>
+    </details>
+
+    <div class="deployment-version">
+      deployment: {pack.versions.slice(-1)[0]}
+    </div>
+  </div>
 </div>
 
 <style>
 .box {
-  display:inline-block;
-  background: white;
+  display: inline-block;
+  background: #f9f9f9;
   border: 1px solid black;
   border-radius: 10px;
   margin: 10px 10px;
   padding: 10px;
+  display: flex;
+  min-width:200px;
   width: 300px;
-  min-height:100px;
-  display:flex;
+
 }
-  .icon {
-    min-width:60px;
-    height:60px;
-    background:#f7f7f7;
-    margin:10px;
-  }
-  summary {
-    flex:1;
-    
-  }
+
+.icon {
+  min-width: 60px;
+  height: 60px;
+  background: #f7f7f7;
+  margin: 10px;
+}
+details {
+  flex: 1;
+}
+.right-side {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+}
+.deployment-version {
+  text-align: right;
+  font-size: 12px;
+}
 </style>
